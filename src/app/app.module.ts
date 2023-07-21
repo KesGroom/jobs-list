e78a389eb89bef9d89a12a9917794acb3f6a5412
kevin.sanchez1 <kevin.sanchez@agrodatai.com>
2023-07-21 15:43:45 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { SearchComponent } from './modules/search/search.component';
+import { JobDescriptionComponent } from './modules/job-description/job-description.component';
+import { JobsListComponent } from './modules/jobs-list/jobs-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutes } from './app.routing';
+import { PopupComponent } from './components/popup/popup.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    JobDescriptionComponent,
+    JobsListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutes,
+    PopupComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
